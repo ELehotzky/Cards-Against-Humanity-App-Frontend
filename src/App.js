c0394c32c8ed34e986ad0939288a8e09ad69eb0a
RebGov <becci.govert@gmail.com>
2018-10-02 11:06:05 -0500

    begin making pages html css js components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import cardsAgainstHumanityLogo from './photos/cardsAgainstHumanityLogo.png';
 import './App.css';
-import { Button } from 'reactstrap';
+// import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={cardsAgainstHumanityLogo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Cards Against Humanity</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
       </div>
     );
---
+++ src/Components/GamePage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import GameWelcomePage from '.GameWelcomePage';
+
+
+export default class GamePage extends Component {
+  constructor(){
+    super();
+    this.state = {
+      players:[],
+      
+
+    }
+  }
+  render()
+  return{
+
+  }
+}
