534bce1d2a24c5f5d81bd95b757c329eae3038bf
Alejandro Aguilar Jr <a.a.j@live.com>
2018-10-03 15:01:40 -0500

    Revert "Alex"

--- src/Components/User.js
+++ src/Components/User.js
@@ -7,53 +7,23 @@ export default class User extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user:{
-        game_id:null,
-        name:null
-      },
-      cards:[]
+      game_id:null,
+      name:null,
+      points:0
     };
   }
 
   addUser =() =>{
-    api.subscribe('Users', 'create', this.state.user, data => {
-      this.userResp(data)
-    })
-    //create user cards
-  }
-
-  userResp = (data) =>{
-    if (data.id){
-      for(let i =1; i<=7; i++){
-        api.trigger('PlayedCards', 'create', {user_id:data.id, white_card_id:i, round_id:1})
-      }
-
-      api.subscribe('Users', 'show', {id:data.id}, data =>{
-        this.setState({cards:data.user.played_cards})
-      })
-
-    }
-    else{
-      alert(data)
-    }
+    api.trigger('Users', 'create', this.state)
   }
 
 
   render() {
-    console.log(this.state)
     return (
       <div>
-        {this.state.cards.length < 1 ?
-          <div>
-            <input placeholder="game ID" onChange={e => this.setState({user:{...this.state.user, game_id:e.target.value}})}></input>
-            <input placeholder="Name" onChange={e => this.setState({user:{...this.state.user, name:e.target.value}})}></input>
-            <button onClick={this.addUser}>submit</button>
-          </div>
-          :
-          <ul>
-            {this.state.cards.map(card => <li>{card.whiteCard}</li>)}
-          </ul>
-        }
+        <input placeholder="game ID" onChange={e => this.setState({game_id:e.target.value})}></input>
+        <input placeholder="Name" onChange={e => this.setState({name:e.target.value})}></input>
+        <Button onClick={this.addUser}>Submit</Button>
       </div>
 
     );
